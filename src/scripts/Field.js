b4449c163fde33929b1c4e5f20a00fff7a8c7082
Volha29 <161131142+Volha29@users.noreply.github.com>
2026-05-09 17:04:58 +0300

    Final2

--- src/scripts/Field.js
+++ src/scripts/Field.js
@@ -96,7 +96,7 @@ export default class Field {
                 };
             }
         });
-       this.game.audio.playSound('gameOver');
+        if (!this.game.audio.isMusicPlay) this.game.audio.playSound('gameOver');
         if (promises.length > 0) {
             await Promise.all(promises);
         }
